f30ee419b64b70bc9fd7139f034bb4675f8d70a0
Racheal Amuneke <rachealsamuel708@gmail.com>
2022-05-03 17:38:19 -0400

    controllers created

---
+++ db/migrate/20220503212351_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :image_url
+      t.string :genre
+      t.string :plot
+      t.string :total_time
+
+      t.timestamps
+    end
+  end
+end
