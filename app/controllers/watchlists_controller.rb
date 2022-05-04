f176ee1e4cfbfceda9219bab7f771c274dde383e
Racheal Amuneke <rachealsamuel708@gmail.com>
2022-05-04 16:25:53 -0400

    watchlist routes created

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -1,2 +1,23 @@
 class WatchlistsController < ApplicationController
+
+    def index
+        watchlists = Movie.all
+        render json: watchlists
+    end
+
+    def show
+        watchlist = Watchlist.find(params[:id])
+        render json: watchlist
+    end
+
+    def create
+        watchlist = Watchlist.create!(watchlist_params)
+        render json: watchlist, status: :created
+    end
+
+    private
+
+    def watchlist_params
+        params.permit(:user_id, :movie_id, :rating, :comment)
+    end
 end
